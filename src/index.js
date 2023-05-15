fe29b95fa95f367d71841f53035ed6dae5de61dd
sashazabor8 <106959082+sashazabor8@users.noreply.github.com>
2023-05-16 00:12:29 +0300

    add 1 homeWork

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from 'components/App';
-import Statistics from 'components/Statistics';
+
 import './index.css';
 console.log(123);
 console.log(counter(5, 4));
